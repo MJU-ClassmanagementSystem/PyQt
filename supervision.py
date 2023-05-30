746c791ca3e1f9da8069bc012cf97a8999c8fccb
BHyeonKim <rlaqhguse@gmail.com>
2023-05-31 00:27:44 +0900

    얼굴인식 함수를 클래스로 변환

---
+++ supervision.py
@@ -0,0 +1,137 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QPushButton
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtCore import Qt
+import cv2
+import dlib
+import numpy as np
+import tensorflow as tf
+from keras.models import load_model
+from video_thread import VideoThread
+
+
+class Supervision(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.face_recognition_model = dlib.face_recognition_model_v1('dlib_face_recognition_resnet_model_v1.dat')
+        self.landmark_detector = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+        self.known_embeddings = []
+        self.known_labels = []
+        self.blink_counts = []
+
+        self.face_detector = dlib.get_frontal_face_detector()
+
+        self.emotion_model = load_model('emotion_model.hdf5')
+        self.emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+
+        self.th = VideoThread(self)
+        self.th.change_pixmap_signal.connect(self.update_image)
+        self.th.start()
+
+        self.initUI()
+
+    def initUI(self):
+        self.image_label = QLabel(self)
+        self.image_label.resize(640, 480)
+
+        self.main_menu_button = QPushButton("Main Menu", self)
+        # Connect the main menu button to an appropriate slot...
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.image_label)
+        layout.addWidget(self.main_menu_button)
+
+        self.setLayout(layout)
+
+    def add_known_face_embedding(self, embedding, label):
+        self.known_embeddings.append(embedding)
+        self.known_labels.append(label)
+        self.blink_counts.append(0)
+
+    def load_face_embeddings(self, images):
+        for image in images:
+            person_image = cv2.imread(image)
+            person_dlib_frame = cv2.cvtColor(person_image, cv2.COLOR_BGR2RGB)
+            person_faces = self.face_detector(person_dlib_frame)
+            person_landmarks = self.landmark_detector(person_dlib_frame, person_faces[0])
+            person_embedding = self.face_recognition_model.compute_face_descriptor(person_dlib_frame, person_landmarks)
+            label = image.split('0')[0]
+            self.add_known_face_embedding(person_embedding, label)
+
+    @staticmethod
+    def calculate_eye_aspect_ratio(eye_landmarks):
+        eye_landmarks = np.array([[p.x, p.y] for p in eye_landmarks])
+
+        horizontal_dist1 = np.linalg.norm(eye_landmarks[0] - eye_landmarks[3])
+        horizontal_dist2 = np.linalg.norm(eye_landmarks[1] - eye_landmarks[2])
+        horizontal_length = (horizontal_dist1 + horizontal_dist2) / 2
+
+        vertical_dist1 = np.linalg.norm(eye_landmarks[5] - eye_landmarks[1])
+        vertical_dist2 = np.linalg.norm(eye_landmarks[4] - eye_landmarks[2])
+        vertical_length = (vertical_dist1 + vertical_dist2) / 2
+
+        aspect_ratio = vertical_length / horizontal_length
+
+        return aspect_ratio
+
+    def update_image(self, cv_img):
+        dlib_frame = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+        faces = self.face_detector(dlib_frame)
+
+        for face in faces:
+            landmarks = self.landmark_detector(dlib_frame, face)
+            embedding = self.face_recognition_model.compute_face_descriptor(dlib_frame, landmarks)
+
+            distances = []
+            for known_embedding in self.known_embeddings:
+                distance = np.linalg.norm(np.array(embedding) - np.array(known_embedding))
+                distances.append(distance)
+
+            min_distance_idx = np.argmin(distances)
+            min_distance = distances[min_distance_idx]
+
+            if min_distance <= 0.4:
+                label = self.known_labels[min_distance_idx]
+            else:
+                label = 'Unknown'
+
+            left, top, right, bottom = face.left(), face.top(), face.right(), face.bottom()
+            cv2.rectangle(cv_img, (left, top), (right, bottom), (0, 255, 0), 2)
+            cv2.putText(cv_img, f"{label} {min_distance}", (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+            face_img = cv_img[top:bottom, left:right]
+            gray_face_img = cv2.cvtColor(face_img, cv2.COLOR_BGR2GRAY)
+
+            resized_img = cv2.resize(gray_face_img, (64, 64), interpolation=cv2.INTER_AREA)
+            img_array = tf.keras.preprocessing.image.img_to_array(resized_img)
+            img_array = np.expand_dims(img_array, axis=0)
+            img_array /= 255
+            predictions = self.emotion_model.predict(img_array)
+            max_index = np.argmax(predictions[0])
+            emotion = self.emotion_labels[max_index]
+            emotions = {k: v for k, v in zip(self.emotion_labels, predictions[0])}
+
+            cv2.putText(cv_img, emotion, (left, top - 40), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+
+        qt_img = self.convert_cv_qt(cv_img)
+        self.image_label.setPixmap(qt_img)
+
+    @staticmethod
+    def convert_cv_qt(cv_img):
+        rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+        h, w, ch = rgb_image.shape
+        bytes_per_line = ch * w
+        convert_to_Qt_format = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
+        p = convert_to_Qt_format.scaled(640, 480, Qt.KeepAspectRatio)
+        return QPixmap.fromImage(p)
+
+    def run(self):
+        self.load_face_embeddings(['person1.jpg', 'person2.jpg'])
+        self.show()
+
+
+app = QApplication([])
+ex = Supervision()
+ex.run()
+app.exec_()
--- video_thread.py
+++ video_thread.py
@@ -1,14 +1,23 @@
 import cv2
-from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtCore import QThread, pyqtSignal, QTimer
+import numpy as np
+
+
 
 class VideoThread(QThread):
-    change_pixmap_signal = pyqtSignal(object)
+    change_pixmap_signal = pyqtSignal(np.ndarray)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.cap = cv2.VideoCapture(0)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.capture_frame)
+        self.timer.start(1000/30)  # Capture at 30 fps
 
     def run(self):
-        self.cap = cv2.VideoCapture(0)
-        while True:
-            ret, cv_img = self.cap.read()
-            if ret:
-                self.change_pixmap_signal.emit(cv_img)
-            else:
-                print("Unable to access the camera.")
+        self.exec_()
+
+    def capture_frame(self):
+        ret, cv_img = self.cap.read()
+        if ret:
+            self.change_pixmap_signal.emit(cv_img)
