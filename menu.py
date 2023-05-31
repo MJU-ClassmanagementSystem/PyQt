fae9dc6161e06c3f32a7f19140c9e8de4e0c0cc2
bluesun147 <bluesun147@naver.com>
2023-05-31 16:15:25 +0900

    로그인 기능 추가

--- menu.py
+++ menu.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel
 from register import Register
 from attendance import Attendance
 from supervision import Supervision
@@ -11,6 +11,9 @@ class MainMenu(QWidget):
 
     def initUI(self):
         self.setWindowTitle(self.title)
+        self.setGeometry(500, 500, 500, 200)
+
+        self.label_user_id = QLabel("ID here! ID here! ID here! ID here!", self)  # 사용자 ID를 보여줄 레이블
 
         self.register_button = QPushButton("Register", self)
         self.register_button.clicked.connect(self.open_register)
@@ -43,7 +46,17 @@ class MainMenu(QWidget):
         self.supervision = Supervision(self)
         self.supervision.show()
 
-app = QApplication([])
-ex = MainMenu()
-ex.show()
-app.exec_()
+    def set_user_id(self, user_id):
+        self.user_id = user_id
+        self.label_user_id.setText("id: " + self.user_id)
+
+# app = QApplication([])
+# ex = MainMenu()
+# ex.show()
+# app.exec_()
+
+if __name__ == "__main__":
+    app = QApplication([])
+    ex = MainMenu()
+    ex.show()
+    app.exec_()
